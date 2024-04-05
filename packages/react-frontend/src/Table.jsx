8a3b36a3940f067099ab17c7787715f08e9b2e1b
RayLui2 <luiray28@gmail.com>
2024-04-05 10:50:05 -0700

    step 5

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -15,33 +15,24 @@ function TableHeader() {
   )
 }
 
-function TableBody() {
-  return (
-    <tbody>
-      <tr>
-        <td>Charlie</td>
-        <td>Janitor</td>
+function TableBody(props) {
+  /* use props to map an array to return a table row for each object in the array */
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
       </tr>
-      <tr>
-        <td>Mac</td>
-        <td>Bouncer</td>
-      </tr>
-      <tr>
-        <td>Dee</td>
-        <td>Aspiring actress</td>
-      </tr>
-      <tr>
-        <td>Dennis</td>
-        <td>Bartender</td>
-      </tr>
-    </tbody>
-  )
+    )
+  })
+  return <tbody>{rows}</tbody>
 }
-function Table() {
+function Table(props) {
   return (
+    /* need to be able to access the data(characterData) on this side */
     <table>
       <TableHeader />
-      <TableBody />
+      <TableBody characterData={props.characterData} />
     </table>
   )
 }

bed5206939f286c9b8adcd656d18a58e20e28e67
RayLui2 <luiray28@gmail.com>
2024-04-05 09:54:04 -0700

    step 4 -> Props

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -1,36 +1,47 @@
 // src/Table.jsx
 import React from 'react'
 
-function Table() {
+/* thead = table heading, tr = table row, td = table data */
+/* breaking up table into serparate compononents */
+
+function TableHeader() {
   return (
-    // thead = table heading, tr = table row, td = table data
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Charlie</td>
-          <td>Janitor</td>
-        </tr>
-        <tr>
-          <td>Mac</td>
-          <td>Bouncer</td>
-        </tr>
-        <tr>
-          <td>Dee</td>
-          <td>Aspiring actress</td>
-        </tr>
-        <tr>
-          <td>Dennis</td>
-          <td>Bartender</td>
-        </tr>
-      </tbody>
-    </table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
   )
 }
 
-export default Table
+function TableBody() {
+  return (
+    <tbody>
+      <tr>
+        <td>Charlie</td>
+        <td>Janitor</td>
+      </tr>
+      <tr>
+        <td>Mac</td>
+        <td>Bouncer</td>
+      </tr>
+      <tr>
+        <td>Dee</td>
+        <td>Aspiring actress</td>
+      </tr>
+      <tr>
+        <td>Dennis</td>
+        <td>Bartender</td>
+      </tr>
+    </tbody>
+  )
+}
+function Table() {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody />
+    </table>
+  )
+}

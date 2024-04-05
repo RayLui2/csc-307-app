36314d4fc916afaede0d34f439cea84362808045
RayLui2 <luiray28@gmail.com>
2024-04-05 10:51:01 -0700

    Step 5

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import Table from './Table'
 
+/* creating array of objects to pass data down to children */
+const characters = [
+  {
+    name: 'Charlie',
+    job: 'Janitor',
+  },
+  {
+    name: 'Mac',
+    job: 'Bouncer',
+  },
+  {
+    name: 'Dee',
+    job: 'Aspring actress',
+  },
+  {
+    name: 'Dennis',
+    job: 'Bartender',
+  },
+]
+
 function MyApp() {
   return (
     /* container is a style defined in React */
+    /* passing data in to child through a property "chatacterData" */
     <div className="container">
-      <Table />
+      <Table characterData={characters} />
     </div>
   )
 }

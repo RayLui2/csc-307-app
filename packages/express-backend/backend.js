dedc50c5164fae2dbb3112c571a8665e9eb008cb
RayLui2 <luiray28@gmail.com>
2024-04-13 18:07:37 -0700

    Step 6 DONE

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,6 +40,19 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
   users['users_list'].find((user) => user['id'] === id)
 
+const addUser = (user) => {
+  users['users_list'].push(user)
+  return user
+}
+
+app.use(express.json())
+
+app.post('/users', (req, res) => {
+  const userToAdd = req.body
+  addUser(userToAdd)
+  res.send()
+})
+
 app.get('/users/:id', (req, res) => {
   const id = req.params['id'] //or req.params.id
   let result = findUserById(id)
@@ -49,8 +62,6 @@ app.get('/users/:id', (req, res) => {
     res.send(result)
   }
 })
-
-app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')

b3edbcf52d574862192af344368a1b90447b78b7
RayLui2 <luiray28@gmail.com>
2024-04-14 13:25:55 -0700

    DELETE Done for 7

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -45,7 +45,38 @@ const addUser = (user) => {
   return user
 }
 
+const deleteUserById = (id) => {
+  // Find specific user by ID in users
+  const userToDel = users.users_list.find((user) => user.id === id)
+  // Check if the user exists
+  if (userToDel) {
+    // Get the index of user from users_list
+    const index = users.users_list.indexOf(userToDel)
+    // Remove the user: starts at index, removes 1 from there(the user)
+    if (index !== -1) {
+      users.users_list.splice(index, 1)
+      return true
+    }
+  }
+  // no user with specified ID was found
+  return false
+}
+
 app.use(express.json())
+
+app.delete('/users/:id', (req, res) => {
+  const id = req.params['id']
+  const deleted_user = deleteUserById(id)
+  // if we successfully deleted a user
+  if (deleted_user) {
+    // this response indicates delete was successful
+    res.status(204).end()
+  }
+  // could not find the ID of the user to delete
+  else {
+    res.status(404).send('Resource not found.')
+  }
+})
 
 app.post('/users', (req, res) => {
   const userToAdd = req.body
